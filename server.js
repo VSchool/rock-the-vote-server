4ad1112a103d2475b76878a57806126770f96494
Jordan Burger <jordanburger22@gmail.com>
2024-06-18 17:02:16 +0000

    added react

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const app  = express()
+const app = express()
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 require('dotenv').config()
-const {expressjwt} = require('express-jwt')
+const { expressjwt } = require('express-jwt')
 
 app.use(express.json())
 app.use(morgan('dev'))
 
-async function connectToDb(){
+async function connectToDb() {
     try {
         await mongoose.connect(process.env.MONGO_URI)
         console.log('connected to db')
@@ -20,15 +20,15 @@ async function connectToDb(){
 connectToDb()
 
 app.use('/api/auth', require('./routes/authRouter'))
-app.use('/api/main', expressjwt({secret: process.env.SECRET, algorithms: ['HS256']}))
+app.use('/api/main', expressjwt({ secret: process.env.SECRET, algorithms: ['HS256'] }))
 app.use('/api/main/issues', require("./routes/issueRouter"))
 
 app.use((err, req, res, next) => {
     console.log(err)
-    if(err.name === "UnauthorizedError"){
+    if (err.name === "UnauthorizedError") {
         res.status(err.status)
     }
-    return res.send({errMsg: err.message})
+    return res.send({ errMsg: err.message })
 })
 
 app.listen(process.env.PORT, () => {
