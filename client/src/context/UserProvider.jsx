58353320c91a469dafce09f43cb789401ccd9f0d
Zak Ward <zakward85@gmail.com>
2024-06-18 12:37:42 -0500

    userContext

---
+++ client/src/context/UserProvider.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from "react"
+
+export const UserContext = React.createContext()
+
+
+export default function UserProvider(props){
+    
+    const initState = {
+        user : {},
+        token : "",
+        issues: []
+    }
+
+    const [userState, setUserState] = useState(initState)
+
+    return (
+        <UserContext.Provider value = {{...userState}}>
+            {props.children}
+        </UserContext.Provider>
+    )
+
+}
