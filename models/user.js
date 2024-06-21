9a30994ac45489a82f4a7e50df1d3260a32ec0d4
Jordan Burger <jordanburger22@gmail.com>
2024-06-21 20:37:43 +0000

    added password security

--- models/user.js
+++ models/user.js
@@ -22,4 +22,35 @@ const userSchema = new Schema({
     }
 })
 
+
+// pre-save hook to encrypt user passwords on signup
+userSchema.pre("save", async function(next){
+    const user = this;
+
+    // Hash the password if it has been modified or is new
+    if (user.isModified('password')) {
+        try {
+            const hash = await bcrypt.hash(user.password, 10);
+            user.password = hash;
+        } catch (err) {
+            return next(err);
+        }
+    }
+  })
+  
+  // method to check encrypted password on login
+  userSchema.methods.checkPassword = async function (passwordAttempt) {
+    try {
+        return await bcrypt.compare(passwordAttempt, this.password);
+    } catch (err) {
+        throw err;
+    }
+};
+
+userSchema.methods.withoutPassword = function () {
+    const user = this.toObject();
+    delete user.password;
+    return user;
+};
+
 module.exports = mongoose.model("User", userSchema)
